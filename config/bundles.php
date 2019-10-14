eaafeea67232680573040b2cd3dfbad4ce002790
Mazarini <mazarini@protonmail.com>
2019-10-14 19:46:59 +0200

    Clean composer

--- config/bundles.php
+++ config/bundles.php
@@ -1,25 +1,9 @@
 <?php
 
-/*
- * Copyright (C) 2019 Mazarini <mazarini@protonmail.com>.
- * This file is part of mazarini/design.
- *
- * mazarini/design is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * mazarini/design is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License
- */
-
 return [
     Mazarini\BootstrapBundle\MazariniBootstrapBundle::class => ['all' => true],
-    Mazarini\DesignBundle\MazariniDesignBundle::class => ['dev' => true, 'test' => true],
+    Mazarini\DesignBundle\MazariniDesignBundle::class => ['all' => true],
+    Mazarini\PackageBundle\MazariniPackageBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
@@ -29,7 +13,4 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
